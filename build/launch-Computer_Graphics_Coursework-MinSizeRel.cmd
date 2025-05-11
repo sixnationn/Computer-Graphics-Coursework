7d27295d516ea07cd1f20da6600940c43187cfce
Harvey Wray <harveywray05@gmail.com>
2025-05-11 14:56:02 +0100

    wtf

---
+++ build/launch-Computer_Graphics_Coursework-MinSizeRel.cmd
@@ -0,0 +1,9 @@
+cd C:/Users/HeyTh/Documents/GitHub/Computer-Graphics-Coursework/source/
+set 
+
+if [%1]==[--debugger] (
+	SHIFT
+)
+
+"C:\Users\HeyTh\Documents\GitHub\Computer-Graphics-Coursework\build\MinSizeRel\Computer_Graphics_Coursework.exe"  
+pause
